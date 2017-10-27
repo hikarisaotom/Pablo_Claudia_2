6e14a387985efb50508e78e872cd91cf31bacc76
Claudia <clau_cortes10@hotmail.com>
2017-10-27 14:06:47 -0600

    Clases_2.3

--- Trabajo_Equipo/src/trabajo_equipo/Clases/Tiendas.java
+++ Trabajo_Equipo/src/trabajo_equipo/Clases/Tiendas.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package trabajo_equipo.Clases;
 
-/**
- *
- * @author Claudia Cortes
- */
 public class Tiendas {
+    protected String Nombre;
+    protected int Num_Piso;
     
 }
